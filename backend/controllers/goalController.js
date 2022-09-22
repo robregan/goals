9fb3d7a563745b55fcbc5c5e96a4167ec26d5126
rob <robert_regan86@yahoo.com>
2022-09-22 12:12:41 -0400

    auth with hashed pass and bearer token finished, goal routes protected

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,13 +1,14 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 
 // get goals
 // route GET /api/goals
 // access Private 
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
 })
 
@@ -21,7 +22,8 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     }) 
     res.status(200).json(goal)
 })
@@ -37,6 +39,19 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
+    const user = await User.findById(req.user.id)
+    //check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // to ensure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, 
         {
             new: true,
@@ -50,7 +65,18 @@ const updateGoal = asyncHandler(async (req, res) => {
 // access Private 
 const deleteGoal = asyncHandler(async (req, res) => {
     const goal = await Goal.findById(req.params.id)
+    const user = await User.findById(req.user.id)
+    //check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
 
+    // to ensure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
